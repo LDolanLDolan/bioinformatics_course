e4f0d590d12aee6a1e0172c45f84294ddbd59230
LDolanLDolan <lita.doolan@me.com>
2025-03-06 16:34:48 +0000

    not tested: updated scripts to make empty dirs and README.md

--- scripts/my-scripts.sh
+++ scripts/my-scripts.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 
+mkdir -p analysis docs data
+
+for my_director in scripts analysis docs data:do
+	touch ${my_directory}/README.md
+	echo "# ${my_directory}" >> ${my_directory}README.md
+done
 
